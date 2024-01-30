1f0f205797eacf20c6c2d4ec5939f23c0d59bb98
Ethan <burdwilliamethan@gmail.com>
2024-01-30 16:43:16 +0000

    Assembly (part 1) basics exercises

--- Unit2_Assembly_Basics/part1_exercises.asm
+++ Unit2_Assembly_Basics/part1_exercises.asm
@@ -24,6 +24,77 @@ asm_main:
         pusha
 
 		; TODO: add code
+        
+
+        ;;; MOVING
+        ; dump_regs 1
+        ; call print_nl
+
+        ; mov eax, 0 ; making EAX = 0
+        ; mov ah, 18 ; AH = 18
+        ; mov al, 0BAH ; AL = BA (hex)
+        ; dump_regs 2
+        ; call print_nl
+
+        ; mov eax, 0BEEFCAFEH ; EAX = BEEFCAFE
+        ; dump_regs 3
+
+        ; mov eax, 3 ; eax = 3
+        ; mov ebx, 4 ; ebx = 4
+        ; dump_regs 1 ; dump registers
+        ; call print_nl ; new line for format
+
+
+        ;;; ADD EXCERCISE
+        ; add eax, ebx ; eax += ebx, eax = 7
+        ; dump_regs 2 ; dump registers
+
+        ; mov eax, 40 ; eax = 40
+        ; mov ebx, 2 ; ebx = 2
+        ; dump_regs 1 ; dump registers
+        ; call print_nl ; new line for format
+
+        ; add ebx, eax ; ebx += eax, ebx = 42
+        ; dump_regs 2 ; dump registers
+
+        ;;; SUBTRACT & INC
+        ; mov eax, 10
+        ; mov ebx, 4
+        ; dump_regs 1
+
+        ; sub eax, ebx ; eax -= ebx (6)
+        ; dump_regs 2
+
+        ; inc ebx ; ebx++ (5)
+        ; dump_regs 3
+
+        ;;; EXERCISE
+        ; mov eax, 16 ; eax = 16
+        ; mov ebx, 4 ; ebx = 4
+        ; dump_regs 1 ; dump registers
+        ; call print_nl
+
+        ; sub eax, ebx ; eax -= ebx (12)
+        ; dump_regs 2 ; dump registers
+        ; call print_nl
+
+        ; mov ecx, eax ; ecx = eax (12)
+        ; dump_regs 3 ; dump registers
+        ; call print_nl
+
+        ; inc ecx ; ecx++ (1)
+        ; dump_regs 4 ; dump registers
+        ; call print_nl
+
+        move eax, 12
+        move ebx, 0
+        dump_regs 1
+
+        add eax, ebx
+        add eax, ebx
+        add eax, ebx
+        add eax, ebx
+        dump_regs 2
 
         popa
         mov     eax, 0            ; return back to C
